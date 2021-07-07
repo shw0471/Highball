637bae3ae244fab752ca6bbb73741373c9e43052
shw0471 <shw0471@naver.com>
2021-07-07 10:30:35 +0900

    🐛 :: change WhiskyDatabase to extend RoomDatabase

--- app/src/main/java/com/semicolon/highball/local/FavoriteWhiskyDatabase.kt
+++ app/src/main/java/com/semicolon/highball/local/FavoriteWhiskyDatabase.kt
@@ -1,12 +1,13 @@
 package com.semicolon.highball.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [FavoriteWhiskyRoomData::class],
     version = 1
 )
-abstract class FavoriteWhiskyDatabase {
+abstract class FavoriteWhiskyDatabase : RoomDatabase() {
 
     abstract fun favoriteWhiskyDao(): FavoriteWhiskyDao
 }
