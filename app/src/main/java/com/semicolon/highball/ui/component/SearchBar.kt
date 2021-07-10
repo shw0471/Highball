ecabba1a578f3d5041d2b14ee9583a8c8e1d12cb
shw0471 <shw0471@naver.com>
2021-07-11 03:50:17 +0900

    ⚡️ :: add SearchBar Component

---
+++ app/src/main/java/com/semicolon/highball/ui/component/SearchBar.kt
@@ -0,0 +1,56 @@
+package com.semicolon.highball.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.*
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchBar(
+    onSearchClick: (keyword: String) -> Unit
+) {
+    var text by remember { mutableStateOf("") }
+
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = { Text("Search") },
+        singleLine = true,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "search"
+            )
+        },
+        modifier = Modifier
+            .padding(horizontal = 20.dp, vertical = 5.dp)
+            .height(50.dp)
+            .fillMaxSize(),
+        shape = RoundedCornerShape(10.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        keyboardActions = KeyboardActions {
+            if(text.isEmpty()) return@KeyboardActions
+            onSearchClick(text)
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview() {
+    SearchBar {
+
+    }
+}
