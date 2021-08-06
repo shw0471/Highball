63c475438282647b1c17f9f9f4bf9266f40716d7
shw0471 <shw0471@naver.com>
2021-08-06 17:12:32 +0900

    fix favorite whisky dao

--- app/src/main/java/com/semicolon/highball/local/FavoriteWhiskyDao.kt
+++ app/src/main/java/com/semicolon/highball/local/FavoriteWhiskyDao.kt
@@ -10,6 +10,9 @@ interface FavoriteWhiskyDao {
     @Query("SELECT * FROM FavoriteWhiskyRoomData")
     fun getFavoriteWhiskyList(): Single<List<FavoriteWhiskyRoomData>>
 
+    @Query("SELECT * FROM FavoriteWhiskyRoomData WHERE id IS :id")
+    fun getFavoriteWhiskyListById(id: Int): Single<List<FavoriteWhiskyRoomData>>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addFavoriteWhisky(favoriteWhisky: FavoriteWhiskyRoomData): Completable
 
